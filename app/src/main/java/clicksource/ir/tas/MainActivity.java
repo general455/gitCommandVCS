ca381e2715bfe202eba2b2f385a655ac99d81c20
mojtaba nekooei <mojtabanekooei@gmail.com>
2019-02-19 07:18:33 +0330

    firs commit from gitbash

---
+++ app/src/main/java/clicksource/ir/tas/MainActivity.java
@@ -0,0 +1,32 @@
+package clicksource.ir.tas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnTas;
+    TextView txtNumber;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtNumber=(TextView) findViewById(R.id.txt_main_number);
+        btnTas=(Button)findViewById(R.id.btn_main_tas);
+
+        btnTas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int number=new Random().nextInt(6);
+                number++;
+                txtNumber.setText(number+"");
+            }
+        });
+    }
+}
